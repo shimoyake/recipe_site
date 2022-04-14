5fdda2d5dadcec09b74c5d25d8117025c7636409
shimoyake <yupu1205@gmail.com>
2022-04-14 10:51:00 +0900

    DBで投稿内容取得できていること確認OK

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Form;
 
 class FormController extends Controller
 {
@@ -11,6 +12,12 @@ class FormController extends Controller
        }
        
        public function savenew (Request $request){
+        $post = new Form;
+        $post->title = $request->title;
+        $post->main = $request->main;
+        $post->save();
+        
+        return redirect ('/create');
        }
 }
 
