0c1a6f2c50e6fb94d6f0c3cd92ea74c8251460c4
shimoyake <yupu1205@gmail.com>
2022-04-14 20:54:42 +0900

    コントローラーに画像投稿アクション追加、storage/app/public内に保存できたこと確認OK

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -16,6 +16,17 @@ class FormController extends Controller
             $post->title = $request->title;
             $post->main = $request->main;
             $post->save();
+
+            // 画像の保存
+            if($request->post_img){
+
+                if($request->post_img->extension() == 'gif' || $request->post_img->extension() == 'jpeg' || 
+                $request->post_img->extension() == 'jpg' || $request->post_img->extension() == 'png'){
+                
+                $request->file('post_img')->storeAs('public/post_img', $post->id.'.'.$request->post_img->extension());
+                }
+
+            }
         
         return redirect ('/index');
        }
