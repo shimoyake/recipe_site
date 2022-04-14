95effced64e17b5bb9b2dacd90d8cc174835f20e
shimoyake <yupu1205@gmail.com>
2022-04-14 10:09:21 +0900

    first viewページトップ作成

---
+++ resources/views/form.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>newsite</title>
+<!-- Styles -->
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div class="wrapper">
+    <div class="header"><h1>投稿ページ</h1></div>
+    <div class="content_wrapper">
+    <div class="content2">
+
+        <form action="/newpostsend" method="post">
+            @csrf
+            <p>タイトル</p>
+            <input type="text" name="title" class="formtitle">
+
+            <p>&nbsp;</p>
+            <p>本文</p>
+            <textarea name="main" cols="40" rows="10"></textarea>
+
+            <p>&nbsp;</p>
+            <input type="submit" class="submitbtn">
+        </form>
+
+    </div>
+    </div>
+    </div>
+</body>
+</html> 
