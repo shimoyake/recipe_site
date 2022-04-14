79d07d82fe5a495a77f59a939625d616c462525c
shimoyake <yupu1205@gmail.com>
2022-04-14 21:04:11 +0900

    個別閲覧ページでも画像憑依できるように変更

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -20,6 +20,16 @@
         <h1>{{$data->title}}</h1>
         <hr>
         <p>{!! nl2br($data->main)!!}</p>
+
+        @if(file_exists(public_path().'/storage/post_img/'. $data->id .'.jpg'))
+            <img src="/storage/post_img/{{ $data->id }}.jpg">
+        @elseif(file_exists(public_path().'/storage/post_img/'. $data->id .'.jpeg'))
+            <img src="/storage/post_img/{{ $data->id }}.jpeg">
+        @elseif(file_exists(public_path().'/storage/post_img/'. $data->id .'.png'))
+            <img src="/storage/post_img/{{ $data->id }}.png">
+        @elseif(file_exists(public_path().'/storage/post_img/'. $data->id .'.gif'))
+            <img src="/storage/post_img/{{ $data->id }}.gif">
+        @endif
         </div>
 
         <div class="footer">
