af2c235f380a3ddf474216e67a5aaf4ffe875c97
shimoyake <yupu1205@gmail.com>
2022-04-14 20:56:21 +0900

    一覧ページに画像表示できるように追加

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -20,6 +20,17 @@
         <h1>{{$datas->title}}</h1>
         <hr>
         <p>{!! nl2br($datas->main) !!}</p>
+
+        @if(file_exists(public_path().'/storage/post_img/'. $datas->id .'.jpg'))
+            <img src="/storage/post_img/{{ $datas->id }}.jpg">
+        @elseif(file_exists(public_path().'/storage/post_img/'. $datas->id .'.jpeg'))
+            <img src="/storage/post_img/{{ $datas->id }}.jpeg">
+        @elseif(file_exists(public_path().'/storage/post_img/'. $datas->id .'.png'))
+            <img src="/storage/post_img/{{ $datas->id }}.png">
+        @elseif(file_exists(public_path().'/storage/post_img/'. $datas->id .'.gif'))
+            <img src="/storage/post_img/{{ $datas->id }}.gif">
+        @endif
+
         </div>
         @endforeach
 
